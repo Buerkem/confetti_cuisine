56b7551b2331fa29586dd030984ccf5187be234e
Eli <elikembuertey@gmail.com>
2022-12-24 23:03:53 -0500

    Added a layout file to serve as base layout

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const port=3000,
 express = require("express"),
+layouts = require("express-ejs-layouts"),
 userController = require("./controllers/userController"),
 errorController = require("./controllers/errorController"),
-ejs = require("ejs")
+ejs = require("ejs"),
 app = express();
 
 app.set("view engine", 'ejs')
 app.use( express.static( "public" ) );
+app.use(layouts);
 
 app.get("/", userController.indexView)
 app.use(errorController.pageNotFoundError)
