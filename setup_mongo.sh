c3e8afcfd1601df35c07132dd908e87c3ab7a0d3
Eli <elikembuertey@gmail.com>
2023-01-20 23:47:39 -0500

    install sudo first and try to install mongodb

--- setup_mongo.sh
+++ setup_mongo.sh
@@ -1,8 +1,8 @@
-whoami
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | apt-key add -
-apt-get install gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-apt-get update
-apt-get install -y mongodb-org
+apt-get install sudo -y
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 systemctl start mongod
